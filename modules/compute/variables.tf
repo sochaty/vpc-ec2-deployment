ecf8aa5a892592f4dc369bfaf0dc21fefd3444d4
Sourish Chakraborty <sourish1978@gmail.com>
2023-01-04 01:45:58 +0800

    added all terraform configurations

---
+++ modules/compute/variables.tf
@@ -0,0 +1,84 @@
+variable "region" {
+  default     = "us-east-1"
+  description = "AWS Region"
+}
+
+# variable "remote_state_bucket" {
+#   description = "Remote bucket for state file"
+# }
+
+# variable "remote_state_key" {
+#   default     = "layer1/infrastructure.tfstate"
+#   description = "Remote state file to read"
+# }
+
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID"
+}
+
+variable "image_id" {
+  type        = string
+  description = "AMI ID"
+}
+
+variable "key_pair_name" {
+  #   default     = "myEC2KeyPair"
+  description = "Key pair for connecting to launched EC2 instances"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 Instance type to launch"
+  type        = string
+}
+
+variable "min_instance_size" {
+  type        = number
+  description = "Minimum number of instances to launch in AutoScaling Group"
+}
+
+variable "max_instance_size" {
+  type        = number
+  description = "Maximum number of instances to launch in AutoScaling Group"
+}
+
+variable "environment" {
+  type    = string
+  default = "Development"
+}
+
+variable "private_subnet_ids" {
+  type        = list(string)
+  description = "Private Subnet IDs"
+}
+
+variable "public_subnet_ids" {
+  type        = list(string)
+  description = "Public Subnet IDs"
+}
+
+variable "win_ec2_image_id" {
+  type = string
+  description = "Windows Test EC2 Private IP"
+}
+
+variable "win_ec2_instance_type" {
+  type = string
+  description = "Windows Test EC2 Private IP"
+}
+
+variable "win_ec2_subnet_id" {
+  type = string
+  description = "Subnet ID for Windows Test EC2"
+}
+
+# variable "win_ec2_private_ip" {
+#   type = string
+#   description = "Windows Test EC2 Private IP"
+# }
+
+# variable "remote_state_key" {
+#   default     = "layer1/infrastructure.tfstate"
+#   description = "Remote state file to read"
+# }
